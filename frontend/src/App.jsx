4f086bc7cbdf0429ebb653df75e72ed8061234bb
Rumon27 <mahamudrasheed@gmail.com>
2026-04-24 16:21:56 +0600

    front end start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App(){
+  return(
+    <div className="min-h-screen bg-gray-100">
+      <h1 className='text-3xl font-bold text-center text-blue-600 pt-10'>
+        CourseCraft
+
+      </h1>
+    </div>
+  )
+}
+
+export default App
